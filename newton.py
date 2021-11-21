24dba8e23641db0f7550f7c7c0fbf796c5f4f241
Pranav Deepak <86701931+PD130102@users.noreply.github.com>
2021-11-21 23:26:25 +0530

    Added Newton's Method

---
+++ newton.py
@@ -0,0 +1,22 @@
+from typing import no_type_check
+import numpy as np
+
+def f(x):
+    return pow(x,3)-12.2*pow(x,2)+7.45*x+42
+
+def f_(x):
+    return 3*pow(x,2)-24.4*x+7.45
+
+x = [0] * 20
+x[0] = float(input("Enter the initail approximation (x0)\n"))
+
+n = int(input("Enter the number or approximations you need \n"))
+
+for i in range (0,n):
+    x[i+1] = x[i] - (float)(f(x[i])/f_(x[i]))
+
+print("{:<8} {:<25} {:<25} {:<25} {:<25} {:<25}".format('K','x[k]','x[k+1]','f(k)','f_(k)', 'f(k)/f_(k)'))
+print('\n')
+
+for i in range (0,n):
+    print("{:<8} {:<25} {:<25} {:<25} {:<25} {:<25}".format(i,x[i],x[i+1],f(x[i]),f_(x[i]),f(x[i])/f_(x[i])))
