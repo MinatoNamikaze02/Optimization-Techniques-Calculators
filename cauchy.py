7b2a7a096829ace82d94d0f652d50b5fb157c5a6
Arjhun S <85065053+MinatoNamikaze02@users.noreply.github.com>
2022-02-07 23:32:22 +0530

    Delete cauchy.py

--- cauchy.py
+++
@@ -1,8 +0,0 @@
-from scipy.optimize import minimize
-
-def f(x):
-    return 4*x[0]**2 - 4*x[0]*x[1] + 2*x[1]**2
-
-x0 = [2,3]
-res = minimize(f, x0, method='CG',options= {"maxiter": 1, 'disp': True})
-print(res.x)
